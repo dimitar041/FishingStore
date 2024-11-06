6e8485ff489f039fef2a5c8e41171022b2383907
dimitar041 <dimitar041@abv.bg>
2024-11-06 17:57:37 +0200

    Rod details added

--- FishingStore.Web/Controllers/RodController.cs
+++ FishingStore.Web/Controllers/RodController.cs
@@ -1,4 +1,5 @@
 ﻿using FishingStore.Data;
+using FishingStore.Data.Models;
 using FishingStore.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ namespace FishingStore.Web.Controllers
 {
     public class RodController(FishingStoreContext dbContext) : BaseController
     {
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var rods = await dbContext
@@ -30,7 +32,43 @@ namespace FishingStore.Web.Controllers
             return View(rods);
         }
 
-        public async Task<IActionResult> Details()
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            Guid rodGuid = Guid.Empty;
+            bool isGuidValid = this.IsGuidValid(id, ref rodGuid);
+
+            if (!isGuidValid)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+                
+            Rod? rod = await dbContext
+                .Rods
+                .FirstOrDefaultAsync(x => x.Guid == rodGuid);
+
+            if (rod == null)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            RodIndexViewModel model = new RodIndexViewModel()
+            {
+                Brand = rod.Brand,
+                Model = rod.Model,
+                Action = rod.Action,
+                Description = rod.Description,
+                FishingType = rod.FishingType.ToString(),
+                Guid = rod.Guid.ToString(),
+                ImageUrl = rod.ImageUrl,
+                Length = rod.Length,
+                Price = rod.Price,
+            };
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit()
         {
             return View();
         }
